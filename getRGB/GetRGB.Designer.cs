b201216bdd52e30a88dd9ce12ab8baa80116cbb5
WongDarren <wongdarren1997@gmail.com>
2019-09-25 01:06:22 -0700

    Upload project

---
+++ getRGB/GetRGB.Designer.cs
@@ -0,0 +1,402 @@
+﻿namespace assignment2
+{
+    partial class GetRGB
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loadButton = new System.Windows.Forms.Button();
+            this.imgBox = new System.Windows.Forms.PictureBox();
+            this.textBox00 = new System.Windows.Forms.TextBox();
+            this.textBox01 = new System.Windows.Forms.TextBox();
+            this.textBox02 = new System.Windows.Forms.TextBox();
+            this.textBox03 = new System.Windows.Forms.TextBox();
+            this.textBox04 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.textBox20 = new System.Windows.Forms.TextBox();
+            this.textBox21 = new System.Windows.Forms.TextBox();
+            this.textBox22 = new System.Windows.Forms.TextBox();
+            this.textBox23 = new System.Windows.Forms.TextBox();
+            this.textBox24 = new System.Windows.Forms.TextBox();
+            this.textBox30 = new System.Windows.Forms.TextBox();
+            this.textBox31 = new System.Windows.Forms.TextBox();
+            this.textBox32 = new System.Windows.Forms.TextBox();
+            this.textBox33 = new System.Windows.Forms.TextBox();
+            this.textBox34 = new System.Windows.Forms.TextBox();
+            this.textBox40 = new System.Windows.Forms.TextBox();
+            this.textBox41 = new System.Windows.Forms.TextBox();
+            this.textBox42 = new System.Windows.Forms.TextBox();
+            this.textBox43 = new System.Windows.Forms.TextBox();
+            this.textBox44 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(12, 12);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(324, 57);
+            this.loadButton.TabIndex = 0;
+            this.loadButton.Text = "Load Image";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // imgBox
+            // 
+            this.imgBox.Location = new System.Drawing.Point(342, 12);
+            this.imgBox.Name = "imgBox";
+            this.imgBox.Size = new System.Drawing.Size(561, 612);
+            this.imgBox.TabIndex = 1;
+            this.imgBox.TabStop = false;
+            this.imgBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.imgBox_MouseMove);
+            // 
+            // textBox00
+            // 
+            this.textBox00.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox00.Location = new System.Drawing.Point(12, 75);
+            this.textBox00.Multiline = true;
+            this.textBox00.Name = "textBox00";
+            this.textBox00.Size = new System.Drawing.Size(60, 60);
+            this.textBox00.TabIndex = 4;
+            this.textBox00.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox01
+            // 
+            this.textBox01.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox01.Location = new System.Drawing.Point(12, 141);
+            this.textBox01.Multiline = true;
+            this.textBox01.Name = "textBox01";
+            this.textBox01.Size = new System.Drawing.Size(60, 60);
+            this.textBox01.TabIndex = 5;
+            this.textBox01.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox02
+            // 
+            this.textBox02.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox02.Location = new System.Drawing.Point(12, 207);
+            this.textBox02.Multiline = true;
+            this.textBox02.Name = "textBox02";
+            this.textBox02.Size = new System.Drawing.Size(60, 60);
+            this.textBox02.TabIndex = 6;
+            this.textBox02.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox03
+            // 
+            this.textBox03.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox03.Location = new System.Drawing.Point(12, 273);
+            this.textBox03.Multiline = true;
+            this.textBox03.Name = "textBox03";
+            this.textBox03.Size = new System.Drawing.Size(60, 60);
+            this.textBox03.TabIndex = 7;
+            this.textBox03.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox04
+            // 
+            this.textBox04.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox04.Location = new System.Drawing.Point(12, 339);
+            this.textBox04.Multiline = true;
+            this.textBox04.Name = "textBox04";
+            this.textBox04.Size = new System.Drawing.Size(60, 60);
+            this.textBox04.TabIndex = 8;
+            this.textBox04.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox10
+            // 
+            this.textBox10.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox10.Location = new System.Drawing.Point(78, 75);
+            this.textBox10.Multiline = true;
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(60, 60);
+            this.textBox10.TabIndex = 9;
+            this.textBox10.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox11
+            // 
+            this.textBox11.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox11.Location = new System.Drawing.Point(78, 141);
+            this.textBox11.Multiline = true;
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(60, 60);
+            this.textBox11.TabIndex = 10;
+            this.textBox11.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox12
+            // 
+            this.textBox12.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox12.Location = new System.Drawing.Point(78, 207);
+            this.textBox12.Multiline = true;
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(60, 60);
+            this.textBox12.TabIndex = 11;
+            this.textBox12.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox13
+            // 
+            this.textBox13.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox13.Location = new System.Drawing.Point(78, 273);
+            this.textBox13.Multiline = true;
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(60, 60);
+            this.textBox13.TabIndex = 12;
+            this.textBox13.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox14
+            // 
+            this.textBox14.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox14.Location = new System.Drawing.Point(78, 339);
+            this.textBox14.Multiline = true;
+            this.textBox14.Name = "textBox14";
+            this.textBox14.Size = new System.Drawing.Size(60, 60);
+            this.textBox14.TabIndex = 13;
+            this.textBox14.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox20
+            // 
+            this.textBox20.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox20.Location = new System.Drawing.Point(144, 75);
+            this.textBox20.Multiline = true;
+            this.textBox20.Name = "textBox20";
+            this.textBox20.Size = new System.Drawing.Size(60, 60);
+            this.textBox20.TabIndex = 14;
+            this.textBox20.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox21
+            // 
+            this.textBox21.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox21.Location = new System.Drawing.Point(144, 141);
+            this.textBox21.Multiline = true;
+            this.textBox21.Name = "textBox21";
+            this.textBox21.Size = new System.Drawing.Size(60, 60);
+            this.textBox21.TabIndex = 15;
+            this.textBox21.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox22
+            // 
+            this.textBox22.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox22.Location = new System.Drawing.Point(144, 207);
+            this.textBox22.Multiline = true;
+            this.textBox22.Name = "textBox22";
+            this.textBox22.Size = new System.Drawing.Size(60, 60);
+            this.textBox22.TabIndex = 16;
+            this.textBox22.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox23
+            // 
+            this.textBox23.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox23.Location = new System.Drawing.Point(144, 273);
+            this.textBox23.Multiline = true;
+            this.textBox23.Name = "textBox23";
+            this.textBox23.Size = new System.Drawing.Size(60, 60);
+            this.textBox23.TabIndex = 17;
+            this.textBox23.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox24
+            // 
+            this.textBox24.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox24.Location = new System.Drawing.Point(144, 339);
+            this.textBox24.Multiline = true;
+            this.textBox24.Name = "textBox24";
+            this.textBox24.Size = new System.Drawing.Size(60, 60);
+            this.textBox24.TabIndex = 18;
+            this.textBox24.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox30
+            // 
+            this.textBox30.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox30.Location = new System.Drawing.Point(210, 75);
+            this.textBox30.Multiline = true;
+            this.textBox30.Name = "textBox30";
+            this.textBox30.Size = new System.Drawing.Size(60, 60);
+            this.textBox30.TabIndex = 19;
+            this.textBox30.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox31
+            // 
+            this.textBox31.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox31.Location = new System.Drawing.Point(210, 141);
+            this.textBox31.Multiline = true;
+            this.textBox31.Name = "textBox31";
+            this.textBox31.Size = new System.Drawing.Size(60, 60);
+            this.textBox31.TabIndex = 20;
+            this.textBox31.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox32
+            // 
+            this.textBox32.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox32.Location = new System.Drawing.Point(210, 207);
+            this.textBox32.Multiline = true;
+            this.textBox32.Name = "textBox32";
+            this.textBox32.Size = new System.Drawing.Size(60, 60);
+            this.textBox32.TabIndex = 21;
+            this.textBox32.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox33
+            // 
+            this.textBox33.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox33.Location = new System.Drawing.Point(210, 273);
+            this.textBox33.Multiline = true;
+            this.textBox33.Name = "textBox33";
+            this.textBox33.Size = new System.Drawing.Size(60, 60);
+            this.textBox33.TabIndex = 22;
+            this.textBox33.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox34
+            // 
+            this.textBox34.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox34.Location = new System.Drawing.Point(210, 339);
+            this.textBox34.Multiline = true;
+            this.textBox34.Name = "textBox34";
+            this.textBox34.Size = new System.Drawing.Size(60, 60);
+            this.textBox34.TabIndex = 23;
+            this.textBox34.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox40
+            // 
+            this.textBox40.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox40.Location = new System.Drawing.Point(276, 75);
+            this.textBox40.Multiline = true;
+            this.textBox40.Name = "textBox40";
+            this.textBox40.Size = new System.Drawing.Size(60, 60);
+            this.textBox40.TabIndex = 24;
+            this.textBox40.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox41
+            // 
+            this.textBox41.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox41.Location = new System.Drawing.Point(276, 141);
+            this.textBox41.Multiline = true;
+            this.textBox41.Name = "textBox41";
+            this.textBox41.Size = new System.Drawing.Size(60, 60);
+            this.textBox41.TabIndex = 25;
+            this.textBox41.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox42
+            // 
+            this.textBox42.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox42.Location = new System.Drawing.Point(276, 207);
+            this.textBox42.Multiline = true;
+            this.textBox42.Name = "textBox42";
+            this.textBox42.Size = new System.Drawing.Size(60, 60);
+            this.textBox42.TabIndex = 26;
+            this.textBox42.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox43
+            // 
+            this.textBox43.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox43.Location = new System.Drawing.Point(276, 273);
+            this.textBox43.Multiline = true;
+            this.textBox43.Name = "textBox43";
+            this.textBox43.Size = new System.Drawing.Size(60, 60);
+            this.textBox43.TabIndex = 27;
+            this.textBox43.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox44
+            // 
+            this.textBox44.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox44.Location = new System.Drawing.Point(276, 339);
+            this.textBox44.Multiline = true;
+            this.textBox44.Name = "textBox44";
+            this.textBox44.Size = new System.Drawing.Size(60, 60);
+            this.textBox44.TabIndex = 28;
+            this.textBox44.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // GetRGB
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.textBox44);
+            this.Controls.Add(this.textBox43);
+            this.Controls.Add(this.textBox42);
+            this.Controls.Add(this.textBox41);
+            this.Controls.Add(this.textBox40);
+            this.Controls.Add(this.textBox34);
+            this.Controls.Add(this.textBox33);
+            this.Controls.Add(this.textBox32);
+            this.Controls.Add(this.textBox31);
+            this.Controls.Add(this.textBox30);
+            this.Controls.Add(this.textBox24);
+            this.Controls.Add(this.textBox23);
+            this.Controls.Add(this.textBox22);
+            this.Controls.Add(this.textBox21);
+            this.Controls.Add(this.textBox20);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox04);
+            this.Controls.Add(this.textBox03);
+            this.Controls.Add(this.textBox02);
+            this.Controls.Add(this.textBox01);
+            this.Controls.Add(this.textBox00);
+            this.Controls.Add(this.imgBox);
+            this.Controls.Add(this.loadButton);
+            this.Name = "GetRGB";
+            this.Text = "Show RGB";
+            this.Load += new System.EventHandler(this.GetRGB_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imgBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.PictureBox imgBox;
+        private System.Windows.Forms.TextBox textBox00;
+        private System.Windows.Forms.TextBox textBox01;
+        private System.Windows.Forms.TextBox textBox02;
+        private System.Windows.Forms.TextBox textBox03;
+        private System.Windows.Forms.TextBox textBox04;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox20;
+        private System.Windows.Forms.TextBox textBox21;
+        private System.Windows.Forms.TextBox textBox22;
+        private System.Windows.Forms.TextBox textBox23;
+        private System.Windows.Forms.TextBox textBox24;
+        private System.Windows.Forms.TextBox textBox30;
+        private System.Windows.Forms.TextBox textBox31;
+        private System.Windows.Forms.TextBox textBox32;
+        private System.Windows.Forms.TextBox textBox33;
+        private System.Windows.Forms.TextBox textBox34;
+        private System.Windows.Forms.TextBox textBox40;
+        private System.Windows.Forms.TextBox textBox41;
+        private System.Windows.Forms.TextBox textBox42;
+        private System.Windows.Forms.TextBox textBox43;
+        private System.Windows.Forms.TextBox textBox44;
+    }
+}
+
---
+++ getRGB/GetRGB.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment2
+{
+    public partial class GetRGB : Form
+    {
+        public GetRGB()
+        {
+            InitializeComponent();
+        }
+
+        private void GetRGB_Load(object sender, EventArgs e)
+        {
+            Bitmap bm = new Bitmap(imgBox.Width, imgBox.Height);
+            using (Graphics g = Graphics.FromImage(bm))
+            {
+                g.Clear(Color.White);
+                imgBox.Image = bm;
+            }
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog imagefileopen = new OpenFileDialog();
+            imagefileopen.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png) | *.jpg; *.jpeg; *.gif; *.bmp; *.png";
+            if (imagefileopen.ShowDialog() == DialogResult.OK)
+            {
+                imgBox.Image = new Bitmap(imagefileopen.FileName);
+                imgBox.Size = imgBox.Image.Size;
+            }
+        }
+
+
+        private void imgBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Array to hold all text boxes
+            TextBox[] tbs = {
+                textBox00, textBox01, textBox02, textBox03, textBox04,
+                textBox10, textBox11, textBox12, textBox13, textBox14,
+                textBox20, textBox21, textBox22, textBox23, textBox24,
+                textBox30, textBox31, textBox32, textBox33, textBox34,
+                textBox40, textBox41, textBox42, textBox43, textBox44,
+            };
+
+            // Create bitmap
+            Bitmap myBitmap = new Bitmap(imgBox.Image);
+
+            
+            int xcoord;         // x coordinate of cursor
+            int ycoord;         // y coordinate of cursor
+            Color color;        // color variable
+            int boxNum = 0;     // box position in array, start at 0
+
+            for (int i = -2; i < 3; i++)
+            // loop through x coordinates
+            {
+                xcoord = e.X + i;       // x coordinate that is i pixels away from cursor
+
+                // check x coordinate bounds
+                if (xcoord <= 0)
+                    xcoord = 0;
+                if (xcoord >= imgBox.Width)
+                    xcoord = imgBox.Width - 1;
+
+                for (int j = -2; j < 3; j++)
+                // loop through y coordinates
+                {
+                    ycoord = e.Y + j;   // y coordinate that is j pixels away from cursor
+
+                    // check y coordinate bounds
+                    if (ycoord <= 0)
+                        ycoord = 0;
+                    if (ycoord >= imgBox.Height)
+                        ycoord = imgBox.Height - 1;
+
+                    // get the color of the pixel
+                    color = myBitmap.GetPixel(xcoord, ycoord);
+
+                    // set textbox background color
+                    tbs[boxNum].BackColor = Color.FromArgb(color.R, color.G, color.B);
+
+                    // clear the textbox of previous text
+                    tbs[boxNum].Clear();
+
+                    // RGB values of coordinates in textbox
+                    tbs[boxNum].AppendText(Environment.NewLine);
+                    tbs[boxNum].AppendText("R: " + color.R + Environment.NewLine);
+                    tbs[boxNum].AppendText("G: " + color.G + Environment.NewLine);
+                    tbs[boxNum].AppendText("B: " + color.B + Environment.NewLine);
+
+                    // Increment to next text box in array
+                    boxNum++;
+                }
+            }
+        }  
+    }
+}
